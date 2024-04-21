f439b0ae5c376a4f55e665be968e23c3bedf76c6
yatin <yatin@DESKTOP-8DD94C9>
2024-04-21 15:48:33 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_bulkubooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
